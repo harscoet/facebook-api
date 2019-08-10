b833aa21a93e7b3b6c645e6551501ac6f7580d3a
Donovan Harscoet <donovan.harscoet@gmail.com>
2019-08-10 23:22:32 +0200

    Missing build

--- dist/actions/getPersonPhotos.d.ts
+++ dist/actions/getPersonPhotos.d.ts
@@ -1,17 +1,21 @@
 import { FacebookRequest } from '../lib/FacebookRequest';
-export declare function getPersonPhotos(request: FacebookRequest): ({ id, }: GetPersonPhotos.Options) => Promise<GetPersonPhotos.Response>;
+export declare function getPersonPhotos(
+  request: FacebookRequest,
+): ({ id }: GetPersonPhotos.Options) => Promise<GetPersonPhotos.Response>;
 export declare namespace GetPersonPhotos {
-    interface Options {
-        id: string;
-    }
-    interface Context {
-        collection_token: string;
-        lst: string;
-        pagelet_token: string;
-    }
-    interface Response {
-        context: Context;
-        photos: string[];
-    }
+  interface Options {
+    id: string;
+  }
+  interface Context {
+    collection_token: string;
+    lst: string;
+    pagelet_token: string;
+  }
+  interface Response {
+    context: Context;
+    photos: string[];
+  }
 }
-export declare type GetPersonPhotos = (options?: GetPersonPhotos.Options) => Promise<GetPersonPhotos.Response>;
+export declare type GetPersonPhotos = (
+  options?: GetPersonPhotos.Options,
+) => Promise<GetPersonPhotos.Response>;
